7ae483b6d9887e0dbf57934cfaedefa7ca0ebd69
Irving Zamora <irvingzamora28@gmail.com>
2024-11-21 08:08:14 -0700

    feat:  Add BackButton component - Introduce a BackButton component for navigation in profile page

---
+++ src/app/celebrity/[slug]/BackButton.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export default function BackButton() {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.back()}
+      className="inline-flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white rounded-full shadow-lg hover:shadow-xl hover:bg-indigo-700 transition-all"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10.707 3.293a1 1 0 010 1.414L6.414 9H17a1 1 0 110 2H6.414l4.293 4.293a1 1 0 11-1.414 1.414l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 0z"
+          clipRule="evenodd"
+        />
+      </svg>
+      Back
+    </button>
+  );
+}
